4b19a296b8cc32d34c683c28f43139fead1e6aac
unpWn4bl3 <smiled1999@outlook.com>
2023-09-11 19:24:53 +0800

    update

--- main.go
+++ main.go
@@ -1,23 +1,73 @@
 package main
 
-import (
-	"algo/tree"
-	"fmt"
-)
-
 func main() {
-	var root *tree.Node
-	nums := []string{"1", "2", "3", "4", "5", "#", "6", "#", "#", "7", "8"}
-	root = tree.InitBinaryTree(nums)
-	foo(root)
 }
 
-func foo(root *tree.Node) {
-	if root == nil {
-		fmt.Println("#")
-		return
+func setZeroes(matrix [][]int) {
+	m, n := len(matrix), len(matrix[0])
+	row0, col0 := false, false
+	for k := range matrix {
+		if matrix[k][0] == 0 {
+			row0 = true
+		}
 	}
-	fmt.Println(root.Val)
-	foo(root.Left)
-	foo(root.Right)
+	for k := range matrix[0] {
+		if matrix[0][k] == 0 {
+			col0 = true
+		}
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = 0
+				matrix[0][j] = 0
+			}
+		}
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if matrix[i][0] == 0 {
+				matrix[i][j] = 0
+			}
+			if matrix[0][j] == 0 {
+				matrix[i][j] = 0
+			}
+		}
+	}
+	if row0 {
+		for k := range matrix {
+			matrix[k][0] = 0
+		}
+	}
+	if col0 {
+		for k := range matrix[0] {
+			matrix[0][k] = 0
+		}
+	}
 }
+
+// func setZeroes(matrix [][]int) {
+// 	m, n := len(matrix), len(matrix[0])
+// 	for i := 0; i < m; i++ {
+// 		for j := 0; j < n; j++ {
+// 			if matrix[i][j] == 0 {
+// 				// 向上
+// 				for x, y := i, j; y >= 0; y-- {
+// 					matrix[x][y] = 0
+// 				}
+// 				// 向下
+// 				for x, y := i, j; y < n; y++ {
+// 					matrix[x][y] = 0
+// 				}
+// 				// 向左
+// 				for x, y := i, j; x >= 0; x-- {
+// 					matrix[x][y] = 0
+// 				}
+// 				//向右
+// 				for x, y := i, j; x < m; x++ {
+// 					matrix[x][y] = 0
+// 				}
+// 			}
+// 		}
+// 	}
+// }
